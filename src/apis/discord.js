9a50fed473b152fd2be40c34128c458b68a10b0b
alexintosh <alessio.d@gmail.com>
2020-10-26 17:26:48 +0100

    Adding discord Notification.

---
+++ src/apis/discord.js
@@ -0,0 +1,18 @@
+const fetch = require('node-fetch');
+const webhookUrl = process.env.DISCORD_WEBHOOK_URL;
+
+console.log('webhookUrl', webhookUrl)
+const discord = async (message) => {
+  let msg = message;
+  console.log(message);
+
+  if (webhookUrl) {
+    const body = JSON.stringify({ content: msg });
+    const headers = { 'Content-Type': 'application/json' };
+    const response = await fetch(webhookUrl, { method: 'POST', body, headers });
+    console.log('STATUS', response.status);
+    console.log('BODY', await response.text());
+  }
+};
+
+exports.notify = discord;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 const ethers = require('ethers');
 const pieABI = require('./abis/pie.json');
 const gasNow = require('./apis/gasnow');  
+const discord = require('./apis/discord');
 
 const provider = ethers.getDefaultProvider('mainnet', {
     infura: process.env.INFURA_KEY,
 });
 
 const pie = process.env.PIE.toLowerCase();
+const pieName = process.env.PIENAME;
 
 async function run() {
     console.log('Running');
@@ -18,8 +20,9 @@ async function run() {
     let gasPrices = await gasNow.fetchGasPrice();
 
     console.log('Rapid Gas is:', gasPrices.rapid);
-    let hash = await pool.methods.pokeWeights().send({gasLimit: '100000000000', gasPrice: gasPrices.rapid});
-    console.los('Poke tx hash:', hash);
+    // let hash = await pool.methods.pokeWeights().send({gasLimit: '100000000000', gasPrice: gasPrices.rapid});
+    await discord.notify(`Poking weights of ${pieName} at ${Date.now()}, next pokeing in ${process.env.INTERVAL} seconds.`)
+    console.log('Poke tx hash:', hash);
 }
 
 setInterval(function(){ run()}, process.env.INTERVAL || 60000)
